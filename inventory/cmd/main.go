96f2c6c9e5a64ab847a9af592836ce940e622193
space-wanderer <v.m.puzanov@yandex.ru>
2025-07-21 14:56:14 +0300

    inventory

--- inventory/cmd/main.go
+++ inventory/cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -31,9 +35,41 @@ func main() {
 		}
 	}()
 
+	// Подключение к MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Получаем параметры подключения из переменных окружения
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://inventory-service-user:inventory-service-password@localhost:27017/inventory-service?authSource=admin"
+	}
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatalf("failed to connect to MongoDB: %v", err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Printf("failed to disconnect from MongoDB: %v", err)
+		}
+	}()
+
+	// Проверка подключения
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to ping MongoDB: %v", err)
+	}
+
+	dbName := os.Getenv("MONGODB_DATABASE")
+	if dbName == "" {
+		dbName = "inventory-service"
+	}
+	db := client.Database(dbName)
+
 	s := grpc.NewServer()
 
-	repo := inventoryRepository.NewRepository()
+	repo := inventoryRepository.NewRepository(db)
 	service := inventoryService.NewService(repo)
 	api := inventoryV1API.NewAPI(service)
 
